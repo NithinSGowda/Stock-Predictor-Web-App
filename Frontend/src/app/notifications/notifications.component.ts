67781193941f063f05090bebb4332ea8b7f2c867
Kokila K N <kokilakn13@gmail.com>
2020-11-25 15:51:21 +0530

    Update notifications.component.ts

--- Frontend/src/app/notifications/notifications.component.ts
+++ Frontend/src/app/notifications/notifications.component.ts
@@ -1,89 +1,69 @@
-<div class="panel-header">
-  <div class="header text-center">
-    <h2 class="title stats"><i class="now-ui-icons education_atom spin"></i>  API</h2>
-    <p class="category">Documentation for Stock predictor API
-      <!-- <a href="https://scttcper.github.io/ngx-toastr/" target="_blank">full documentation.</a> -->
-    </p>
-  </div>
-</div>
-<div class="main-content">
-  <div class="row">
-    <div class="col-md-6">
-      <div class="card">
-        <div class="card-header">
-          <h4 class="card-title">API basics</h4>
-        </div>
-        <div class="card-body">
-          <h6> - Available only on GET requests </h6>
-          <div >
-            <a class="text-info" href="/apidoc">
-              - Default endpoint : https://ourdomain.com/api/ </a>
-          </div>
-        </div>
-      </div>
-    </div>
-    <div class="col-md-6">
-      <div class="card">
-        <div class="card-header">
-          <h4 class="card-title">Sample response</h4>
-        </div>
-        <div class="card-body">
-          <div >
-            <a class="text-info" href="/apidoc">
-              Query: https://ourdomain.com/api/aapl </a>
-          </div>
-          <div>
-            <span>Response :</span>
-            <blockquote>
-              <p class="blockquote blockquote-primary">
-                {{'{'}}<br>
-                   "Name":"AAPL",<br>
-                   "Closing Price":[
-                      77.37750244140625,
-                      68.33999633789062,...
-                   ],<br>
-                   "Volume":[
-                      2934370400.0,
-                      3019851200.0,...
-                   ],<br>
-                   "LSTM":[
-                      119.87743377685547,
-                      120.10971069335938,...
-                   ],<br>
-                   "Lstm Accuracy":94.61319477436646,<br>
-                   "Arima":[
-                      115.07233307200813,
-                      115.0228048427293,
-                      115.3210068203414,...
-                   ],<br>
-                   "Arima Accuracy":92.10235028020483<br>
-                {{'}'}}
-              </p>
-            </blockquote>
-          </div>
-        </div>
-      </div>
-    </div>
-    <div class="col-md-12">
-      <div class="card">
-        <div class="card-body">
-          <div class="places-buttons">
-            <div class="row">
-              <div class="col-md-6 ml-auto mr-auto text-center">
-                <h4 class="card-title">
-                  Sample Code
-                  <p class="category">Available in 6 languages</p>
-                </h4>
-              </div>
-            </div>
-            <div class="row">
-              <code ng-include="'xhr.html'">
+import { Component, OnInit } from '@angular/core';
+import { ToastrService } from 'ngx-toastr';
 
-              </code>
-            </div>
-          </div>
-        </div>
-      </div>
-    </div>
-  </div>
-</div>
+@Component({
+  selector: 'app-notifications',
+  templateUrl: './notifications.component.html',
+  styleUrls: ['./notifications.component.css']
+})
+export class NotificationsComponent implements OnInit {
+
+  constructor(private toastr: ToastrService) {}
+  showNotification(from, align){
+
+      const color = Math.floor((Math.random() * 5) + 1);
+
+      switch(color){
+        case 1:
+        this.toastr.info('<span class="now-ui-icons ui-1_bell-53"></span> Welcome to <b>Now Ui Dashboard</b> - a beautiful freebie for every web developer.', '', {
+           timeOut: 8000,
+           closeButton: true,
+           enableHtml: true,
+           toastClass: "alert alert-info alert-with-icon",
+           positionClass: 'toast-' + from + '-' +  align
+         });
+        break;
+        case 2:
+        this.toastr.success('<span class="now-ui-icons ui-1_bell-53"></span> Welcome to <b>Now Ui Dashboard</b> - a beautiful freebie for every web developer.', '', {
+           timeOut: 8000,
+           closeButton: true,
+           enableHtml: true,
+           toastClass: "alert alert-success alert-with-icon",
+           positionClass: 'toast-' + from + '-' +  align
+         });
+        break;
+        case 3:
+        this.toastr.warning('<span class="now-ui-icons ui-1_bell-53"></span> Welcome to <b>Now Ui Dashboard</b> - a beautiful freebie for every web developer.', '', {
+           timeOut: 8000,
+           closeButton: true,
+           enableHtml: true,
+           toastClass: "alert alert-warning alert-with-icon",
+           positionClass: 'toast-' + from + '-' +  align
+         });
+        break;
+        case 4:
+        this.toastr.error('<span class="now-ui-icons ui-1_bell-53"></span> Welcome to <b>Now Ui Dashboard</b> - a beautiful freebie for every web developer.', '', {
+           timeOut: 8000,
+           enableHtml: true,
+           closeButton: true,
+           toastClass: "alert alert-danger alert-with-icon",
+           positionClass: 'toast-' + from + '-' +  align
+         });
+         break;
+         case 5:
+         this.toastr.show('<span class="now-ui-icons ui-1_bell-53"></span> Welcome to <b>Now Ui Dashboard</b> - a beautiful freebie for every web developer.', '', {
+            timeOut: 8000,
+            closeButton: true,
+            enableHtml: true,
+            toastClass: "alert alert-primary alert-with-icon",
+            positionClass: 'toast-' + from + '-' +  align
+          });
+        break;
+        default:
+        break;
+      }
+  }
+  ngOnInit() {
+  }
+
+}
