62e9b11fa9ca682bb202feb723f0ef4a97514604
Kokila K N <kokilakn13@gmail.com>
2020-11-25 15:23:13 +0530

    Update chart.py

--- Backend/PYTHON/chart.py
+++ Backend/PYTHON/chart.py
@@ -4,7 +4,7 @@ import sys
 from datetime import datetime
 
 company=sys.argv[1]
-df = yf.download(company,start='2020-01-01')
+df = yf.download(company,start='2015-01-01')
 df.reset_index(inplace=True)
 df['Date']=df['Date'].astype(str)
 df.to_json("PYTHON/"+company+"prices.json",orient='records',date_format = 'iso')
